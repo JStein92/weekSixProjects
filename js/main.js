2cb1e3505620de59b1d4e5a3e797e1fb68acdd20
JStein92 <jonathanstein@live.com>
2017-05-26 11:32:44 -0700

    add 3 more projects

--- js/main.js
+++ js/main.js
@@ -35,5 +35,39 @@ $(function() {
     $('#upperCaseReturn').text(inputUpperCase);
     event.preventDefault();
   });
+  //productOrder
+  $('#formProductOrder form').submit(function(event){
+    var inputUserName = $('#orderUserName').val();
+    var inputUserAddress = $('#orderUserAddress').val();
 
+    $('#reciept').show();
+
+    $('.orderUserName').text(inputUserName);
+    $('.orderUserAddress').text(inputUserAddress);
+    event.preventDefault();
+  });
+  //booking
+  $('#bookingForm form').submit(function(event){
+    var inputUserName = $('#bookingUserName').val();
+    var inputDate = $('#date').val();
+    var inputStartTime = $('#startTime').val();
+    var inputEndtime = $('#endTime').val();
+
+    $('#bookingConfirmation').show();
+
+    $('.bookingUserName').text(inputUserName);
+    $('.date').text(inputDate);
+    $('.startTime').text(inputStartTime);
+    $('.endTime').text(inputEndtime);
+    $('#bookingForm').hide();
+    event.preventDefault();
+  });
+  //questions
+  $('#buttonQuestions').click(function(){
+    var userNameInput = $('#questions #userName').val();
+    var numberInput = $('#number').val();
+    var colorInput = $('#color').val();
+
+    $('#buttonQuestions').after('<br>'+userNameInput + " " + numberInput + " " + colorInput);
+  });
 });
